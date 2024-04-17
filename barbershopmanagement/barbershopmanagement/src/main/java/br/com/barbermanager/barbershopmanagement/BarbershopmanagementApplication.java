0c838287c36a00860d55fdbb8c96707bbd19343a
oLucasNascimento <lucasf.ferreira@bandtec.com.br>
2024-04-17 20:47:32 -0300

    Projeto criado

---
+++ barbershopmanagement/barbershopmanagement/src/main/java/br/com/barbermanager/barbershopmanagement/BarbershopmanagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.barbermanager.barbershopmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbershopmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbershopmanagementApplication.class, args);
+	}
+
+}
---
+++ barbershopmanagement/barbershopmanagement/src/test/java/br/com/barbermanager/barbershopmanagement/BarbershopmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.barbermanager.barbershopmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbershopmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

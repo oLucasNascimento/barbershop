c69ae5899832d917b3b954e5a4a87241b1ad59f4
oLucasNascimento <lucasf.ferreira@bandtec.com.br>
2024-10-08 14:59:48 -0300

    refactored the way to catch and throw exceptions

--- barbershopmanagement/barbershopmanagement/src/main/java/br/com/barbermanager/barbershopmanagement/exception/handler/RestExceptionHandler.java
+++ barbershopmanagement/barbershopmanagement/src/main/java/br/com/barbermanager/barbershopmanagement/exception/handler/RestExceptionHandler.java
@@ -1,55 +1,154 @@
 package br.com.barbermanager.barbershopmanagement.exception.handler;
 
 import br.com.barbermanager.barbershopmanagement.exception.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
-    private ResponseEntity<RestErrorMessage> runTimeException(RuntimeException exception){
-        RestErrorMessage errorMessage = new RestErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
+    private ResponseEntity<RestErrorMessage> runTimeException(RuntimeException exception, WebRequest request){
+        String timestamp = LocalDateTime.now().toString();
+        String path = request.getDescription(false).replace("uri=", "");
+        String errorCode = "RUNTIME_ERROR"; // Ou algum código apropriado
+
+        RestErrorMessage errorMessage = new RestErrorMessage(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                exception.getMessage(),
+                timestamp,
+                path,
+                errorCode
+        );
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
     }
 
     @ExceptionHandler(NullPointerException.class)
-    private ResponseEntity<RestErrorMessage> nullPointerException(NullPointerException exception){
-        RestErrorMessage errorMessage = new RestErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
+    private ResponseEntity<RestErrorMessage> nullPointerException(NullPointerException exception, WebRequest request){
+        String timestamp = LocalDateTime.now().toString();
+        String path = request.getDescription(false).replace("uri=", "");
+        String errorCode = "NULL_POINTER_ERROR"; // Ou algum código apropriado
+
+        RestErrorMessage errorMessage = new RestErrorMessage(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                exception.getMessage(),
+                timestamp,
+                path,
+                errorCode
+        );
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
     }
 
     @ExceptionHandler(NotFoundException.class)
-    private ResponseEntity<RestErrorMessage> notFoundException(NotFoundException exception){
-        RestErrorMessage errorMessage = new RestErrorMessage(HttpStatus.NOT_FOUND, exception.getMessage());
+    private ResponseEntity<RestErrorMessage> notFoundException(NotFoundException exception, WebRequest request){
+        String timestamp = LocalDateTime.now().toString();
+        String path = request.getDescription(false).replace("uri=", "");
+        String errorCode = "NOT_FOUND_ERROR";
+        RestErrorMessage errorMessage = new RestErrorMessage(
+                HttpStatus.NOT_FOUND,
+                exception.getMessage(),
+                timestamp,
+                path,
+                errorCode
+        );
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
     }
 
     @ExceptionHandler(AlreadyExistsException.class)
-    private ResponseEntity<RestErrorMessage> alreadyExistsException(AlreadyExistsException exception){
-        RestErrorMessage errorMessage = new RestErrorMessage(HttpStatus.CONFLICT, exception.getMessage());
+    private ResponseEntity<RestErrorMessage> alreadyExistsException(AlreadyExistsException exception, WebRequest request){
+        String timestamp = LocalDateTime.now().toString();
+        String path = request.getDescription(false).replace("uri=", "");
+        String errorCode = "ALREADY_EXISTS_ERROR"; // Ou algum código apropriado
+
+        RestErrorMessage errorMessage = new RestErrorMessage(
+                HttpStatus.CONFLICT,
+                exception.getMessage(),
+                timestamp,
+                path,
+                errorCode
+        );
         return ResponseEntity.status(HttpStatus.CONFLICT).body(errorMessage);
     }
 
     @ExceptionHandler(BadRequestException.class)
-    private ResponseEntity<RestErrorMessage> badRequestException(BadRequestException exception){
-        RestErrorMessage errorMessage = new RestErrorMessage(HttpStatus.BAD_REQUEST, exception.getMessage());
+    private ResponseEntity<RestErrorMessage> badRequestException(BadRequestException exception, WebRequest request){
+        String timestamp = LocalDateTime.now().toString();
+        String path = request.getDescription(false).replace("uri=", "");
+        String errorCode = "BAD_REQUEST_ERROR"; // Ou algum código apropriado
+
+        RestErrorMessage errorMessage = new RestErrorMessage(
+                HttpStatus.BAD_REQUEST,
+                exception.getMessage(),
+                timestamp,
+                path,
+                errorCode
+        );
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
     }
 
     @ExceptionHandler(AlreadyActiveException.class)
-    private ResponseEntity<RestErrorMessage> alreadyActiveException(AlreadyActiveException exception){
-        RestErrorMessage errorMessage = new RestErrorMessage(HttpStatus.CONFLICT, exception.getMessage());
+    private ResponseEntity<RestErrorMessage> alreadyActiveException(AlreadyActiveException exception, WebRequest request){
+        String timestamp = LocalDateTime.now().toString();
+        String path = request.getDescription(false).replace("uri=", "");
+        String errorCode = "ALREADY_ACTIVE_ERROR"; // Ou algum código apropriado
+
+        RestErrorMessage errorMessage = new RestErrorMessage(
+                HttpStatus.CONFLICT,
+                exception.getMessage(),
+                timestamp,
+                path,
+                errorCode
+        );
         return ResponseEntity.status(HttpStatus.CONFLICT).body(errorMessage);
     }
 
     @ExceptionHandler(InactiveException.class)
-    private ResponseEntity<RestErrorMessage> inactiveException(InactiveException exception){
-        RestErrorMessage errorMessage = new RestErrorMessage(HttpStatus.CONFLICT, exception.getMessage());
+    private ResponseEntity<RestErrorMessage> inactiveException(InactiveException exception, WebRequest request){
+        String timestamp = LocalDateTime.now().toString();
+        String path = request.getDescription(false).replace("uri=", "");
+        String errorCode = "INACTIVE_ERROR"; // Ou algum código apropriado
+
+        RestErrorMessage errorMessage = new RestErrorMessage(
+                HttpStatus.CONFLICT,
+                exception.getMessage(),
+                timestamp,
+                path,
+                errorCode
+        );
         return ResponseEntity.status(HttpStatus.CONFLICT).body(errorMessage);
     }
 
+    @Override
+    protected ResponseEntity<java.lang.Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        String defaultMessage = exception.getBindingResult().getFieldErrors().stream()
+                .findFirst() // Obtém o primeiro erro
+                .map(fieldError -> fieldError.getDefaultMessage()) .orElse("Validation error occurred.");
+
+        String timestamp = LocalDateTime.now().toString();
+        String path = request.getDescription(false).replace("uri=", "");
+        String errorCode = "VALIDATION_ERROR"; // Ou algum código apropriado
+
+        RestErrorMessage errorMessage = new RestErrorMessage(
+                HttpStatus.BAD_REQUEST,
+                defaultMessage,
+                timestamp,
+                path,
+                errorCode
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+    }
 }

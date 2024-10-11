2474a6db4da8f56db3fb4da977c2d452a52faf5a
oLucasNascimento <lucasf.ferreira@bandtec.com.br>
2024-10-11 12:01:07 -0300

    included tests in exception classes

--- barbershopmanagement/barbershopmanagement/src/test/java/br/com/barbermanager/barbershopmanagement/BarbershopmanagementApplicationTests.java
+++ barbershopmanagement/barbershopmanagement/src/test/java/br/com/barbermanager/barbershopmanagement/BarbershopmanagementApplicationTests.java
@@ -3,11 +3,14 @@ package br.com.barbermanager.barbershopmanagement;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 @SpringBootTest
 class BarbershopmanagementApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertDoesNotThrow(() -> BarbershopmanagementApplication.main(new String[] {}));
 	}
 
 }
